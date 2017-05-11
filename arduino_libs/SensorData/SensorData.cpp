83cafe8a5a9959facc02a99dac2e71568cb84a2a
pgaleas <patricio.galeas@ufrontera.cl>
2017-05-10 22:41:17 -0300

    adding new cpp file

--- arduino_libs/SensorData/SensorData.cpp
+++ arduino_libs/SensorData/SensorData.cpp
@@ -1,3 +1,9 @@
+/*
+  SensorData.cpp - Library for manage data from different sensors.
+  Project SMARTHARVEST - Universidad de La Frontera
+  Created by Patricio Galeas, May 10, 2017.
+*/
+
 #include SensorData.h
 
 SensorData::SensorData(byte dataType, byte data[6]) {
---
+++ arduino_libs/SensorsQueue/SensorsQueue.cpp
@@ -0,0 +1,7 @@
+/*
+  SensorQueue.cpp - Library for manage data from different sensors.
+  Project SMARTHARVEST - Universidad de La Frontera
+  Created by Patricio Galeas, May 10, 2017.
+*/
+
+#include SensorQueue.h
